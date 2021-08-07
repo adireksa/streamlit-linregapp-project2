6ca7eb4b86c764ff89cf06dfaec03ae4d925aa2d
adireksa <83330543+adireksa@users.noreply.github.com>
2021-08-07 08:38:55 +0800

    updated app.py and requirements.txt

--- app.py
+++ app.py
@@ -71,27 +71,30 @@ st.markdown(filedownload(df_selected_all), unsafe_allow_html=True)
 
 
 def user_input_features():
-        make = st.sidebar.selectbox('Car Brand',(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39))
-        #st.sidebar.write("Keys and Values Dictionary to Decode for Car Brand Inputs:")
-        st.sidebar.write(pd.DataFrame({
-    'Keys & Values': ['0: acura','1: alfa romeo', '2: aston martin', '3: audi','4: bmw','5: buick','6: cadillac','7: chevrolet','8: chrysler','9: dodge','10: fiat','11: ford','12: genesis','13: gmc','14: honda','15: hyundai','16: infiniti','17: jaguar','18: jeep','19: kia','20: lamborghini','21: land rover','22: lexus','23: lincoln','24: maserati','25: mazda','26: mercedes-benz','27: mini','28: mitsubishi','29: nissan','30: porsche','31: ram','32: rolls-royce','33: subaru','34: toyota','35: volkswagen','36: volvo','37: bentley','38: bugatti'],
-}))
-        vehicle_class = st.sidebar.selectbox('Vehicle Class',(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14))
-        st.sidebar.write(pd.DataFrame({
-    'Keys & Values': ['0: suv: small','1: two-seater','2: compact','3: mid-size','4: subcompact','5: minicompact','6: station wagon: small','7: full-size','8: suv: standard','9: pickup truck: small','10: pickup truck: standard','11: minivan','12: van: passenger','13: special purpose vehicle','14: station wagon: mid-size'],    
-}))
-        engine_liters = st.sidebar.slider('Engine Size (L)', 1.0,10.0, 3.0)
-        cylinders = st.sidebar.slider('Cylinders', 2.0,16.0, 6.0)
-        transmission = st.sidebar.selectbox('Transmission',(0,1,2,3,4))
-        st.sidebar.write(pd.DataFrame({
-    'Keys & Values': ['0: Automatic with Select Shift','1: Automated Manual','2: Automatic','3: Manual','4: Continuously Variable',],    
-}))
-        fuel_type = st.sidebar.selectbox('Fuel Type',(0,1,2,3))
-        st.sidebar.write(pd.DataFrame({
-    'Keys & Values': ['0: Premium Gasoline','1: Regular Gasoline','2: Diesel','3: Ethanol(E85)'],    
-}))
+        #make selectbox
+        display_make = ('0: acura','1: alfa romeo', '2: aston martin', '3: audi','4: bmw','5: buick','6: cadillac','7: chevrolet','8: chrysler','9: dodge','10: fiat','11: ford','12: genesis','13: gmc','14: honda','15: hyundai','16: infiniti','17: jaguar','18: jeep','19: kia','20: lamborghini','21: land rover','22: lexus','23: lincoln','24: maserati','25: mazda','26: mercedes-benz','27: mini','28: mitsubishi','29: nissan','30: porsche','31: ram','32: rolls-royce','33: subaru','34: toyota','35: volkswagen','36: volvo','37: bentley','38: bugatti')
+        options_make = list(range(len(display_make)))
+        make = st.sidebar.selectbox("Car Brand", options_make, format_func=lambda x: display_make[x])
 
-        fuel_consumption = st.sidebar.slider('Fuel Consumption', 2.0,27.0, 10.6)
+        #vehicle_class selectbox
+        display_vehicle_class = ('0: suv: small','1: two-seater','2: compact','3: mid-size','4: subcompact','5: minicompact','6: station wagon: small','7: full-size','8: suv: standard','9: pickup truck: small','10: pickup truck: standard','11: minivan','12: van: passenger','13: special purpose vehicle','14: station wagon: mid-size')
+        options_vehicle_class = list(range(len(display_vehicle_class)))
+        vehicle_class = st.sidebar.selectbox("Vehicle Class", options_vehicle_class, format_func=lambda x: display_vehicle_class[x])
+
+        engine_liters = st.sidebar.slider('Engine Size (L)', 1.0,10.0, 3.0)
+
+        cylinders = st.sidebar.slider('Cylinders', 2.0,16.0, 6.0)
+
+        #transmission selectbox
+        display_transmission = ('0: Automatic with Select Shift','1: Automated Manual','2: Automatic','3: Manual','4: Continuously Variable')
+        options_transmission = list(range(len(display_transmission)))
+        transmission = st.sidebar.selectbox("Transmission", options_transmission, format_func=lambda x: display_transmission[x])
+        #fuel type selectbox
+        display_fuel_type = ('0: Premium Gasoline','1: Regular Gasoline','2: Diesel','3: Ethanol(E85)')
+        options_fuel_type = list(range(len(display_fuel_type)))
+        fuel_type = st.sidebar.selectbox("Fuel Type", options_fuel_type, format_func=lambda x: display_fuel_type[x])
+
+        fuel_consumption = st.sidebar.slider('Fuel Consumption Combined (L/100 km)', 2.0,27.0, 10.6)
 
   
         data = {'make':[make],
@@ -149,9 +152,16 @@ st.subheader('Predicted CO2 Emissions (g/km)')
 st.write(prediction)
 
 
+
+
+
+
+
+#st.sidebar.write(transmission_sidebar)
 """
-
-
+        
+        
+        
   
   
   """
